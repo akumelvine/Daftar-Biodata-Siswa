9a0d4e2dc27cca022b2cced7fa44999dc06c60c6
Hadi Rahmah Esa Putra <73008840+hadirep@users.noreply.github.com>
2022-02-10 10:43:11 +0700

    Merubah jumlah perulangan
    merubah baris 11 dan 16. merubah jumlah perulangan menjadi 5 yang sebelumnya 10

--- perulanganFor.java
+++ perulanganFor.java
@@ -8,12 +8,12 @@ public class perulanganFor {
         }
         System.out.println();
 
-        for(int i=0; i<=10; i++) {
+        for(int i=0; i<=5; i++) {
             System.out.print(i + " ");
         }
         System.out.println();
 
-        for(int i=0; i<=10; i+=2) {
+        for(int i=0; i<=5; i+=2) {
             System.out.print(i + " ");
         }
 
