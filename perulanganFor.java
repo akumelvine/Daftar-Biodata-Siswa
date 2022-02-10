636dcedb9b03103167c44fc7fde417c1fd9395d8
Hadi Rahmah Esa Putra <73008840+hadirep@users.noreply.github.com>
2022-02-10 10:41:20 +0700

    Menambahkan File perulanganFor.java
    Menambahkan file perulangan dari laptop

---
+++ perulanganFor.java
@@ -0,0 +1,21 @@
+package KonsepPerulangan05;
+
+public class perulanganFor {
+    public static void main(String[] args) {
+
+        for(int i=10; i>=0; i--) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        for(int i=0; i<=10; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        for(int i=0; i<=10; i+=2) {
+            System.out.print(i + " ");
+        }
+
+    }
+}
